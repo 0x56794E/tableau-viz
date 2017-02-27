30bbbb46bc53ff87026cf5d82ba27caee9d84c61
Vy Thuy <vng0510@gmail.com>
2017-02-26 22:08:23 -0500

    set up axes, scale, triangle and stuff

--- Q3/scatter.js
+++ Q3/scatter.js
@@ -7,56 +7,126 @@ d3.tsv("data.tsv", function (error, data)
 		//If same species has been added before
 		if (!dataMap[d.Species]) 
 		{
-			//console.log("Creating empty array for species " + d.Species);
 			dataMap[d.Species] = [];
 		}
 			
-		dataMap[d.Species].push([d.BodyMass, d.Distribution]);
-		console.log('Pushing pair to array ' + d.Species + ": " + [parseFloat(d.BodyMass), parseFloat(d.Distribution)]);
-		
+		dataMap[d.Species].push([parseFloat(d.BodyMass), parseFloat(d.Distribution)]);
 	});
 	
-	
+	//MUST BE INSIDE the anon. fuc!!!
 	//Charts
 	var w = 600;
 	var h = 500;
+	var padding = 30;
+	
+	//Custom max func
+	var max = d3.max(dataMap.map(function() {
+		return d3.max(array);
+	}));
+	
+	//Scale
+	var xScale = d3.scale.linear()
+						.domain([0, d3.max(dataMap.map(function (array) {
+							return d3.max(array, function (d) 
+									{
+										return d[0];
+									});
+						}))])
+						.range([padding, w - padding * 2]);
+	
+	var yScale = d3.scale.linear()
+						 .domain([0], d3.max(dataMap.map(function (array) {
+							 return d3.max(array, function (d) {
+								 return d[1];
+							 })
+						 })))
+						 .range([h - padding, padding]);
+	
+	//Set up the axes
+	var xAxis = d3.svg.axis()
+					  .scale(xScale)
+					  .orient("bottom")
+					  .ticks(2);
+	
+	var yAxis = d3.svg.axis()
+					 .scale(yScale)
+					 .orient("left")
+					 .ticks(2);
+
+	//Create the chart
 	var svg = d3.select("body")
 				.append("svg")
 				.attr("width", w)
 				.attr("height", h);
 
+	//Use the axes
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(0," + (h - padding) + ")")
+		.call(xAxis);
+	
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(" + padding + ",0)")
+		.call(yAxis);
+	
 	//Chart 1 - Lagomorpha (red circle)
-	var lagoData = svg.selectAll("circle").data(dataMap['Lagomorpha']);
-	lagoData.enter()
+	svg.selectAll("circle")
+		.data(dataMap['Lagomorpha'])
+		.enter()
 		.append("circle")
 		.attr("stroke", "red")
 		.attr("fill", "white")
 		.attr("cx", function (d) {
-			return d[1];
-		})
-		.attr("cy", function (d) {
 			return d[0];
 		})
-		.attr("r", 2);
+		.attr("cy", function (d) {
+			return d[1];
+		})
+		.attr("r", 4);
+	
+	//Chart 2 - Didelphimorphia (blue square)
+	svg.selectAll("rect")
+	.data(dataMap['Didelphimorphia'])
+	.enter()
+	.append("rect")
+	.attr("stroke", "blue")
+	.attr("fill", "none")
+	.attr("x", function (d) {
+		return d[0];
+	})
+	.attr("y", function (d) {
+		return d[1];
+	})
+	.attr("width", 4)
+	.attr("height", 4);
 
-	console.log("done chart 1");
+	//Chart 3 - Dasyuromorphia (green triangle)
+	svg.selectAll("polygon")
+		.data(dataMap['Dasyuromorphia'])
+		.enter()
+			.append("polygon")
+			.attr("fill", "none")
+			.attr("stroke", "green")
+			.attr("points", function (d) {
+				return (d[0] - 2) + ' ' + (d[1] - 2) 
+					+ ',' + (d[0] + 2) + ' ' + (d[1] + 2)
+					+ ',' + d[0] + ' ' + (d[1] + 2); 
+			});
+	
+	svg.selectAll("circle")
+	.data(dataMap['Didelphimorphia'])
+	.enter()
+	.append("circle")
+	.attr("stroke", "blue")
+	.attr("fill", "white")
+	.attr("cx", function (d) {
+		return d[1];
+	})
+	.attr("cy", function (d) {
+		return d[0];
+	})
+	.attr("r", 2);
+
 });
 
-
-
-//Chart 2 - Didelphimorphia (blue square)
-//svg.selectAll("square")
-//	.data(dataMap['Didelphimorphia'])
-//	.enter()
-//	.append("square")
-//	.attr("stroke", "blue")
-//	.attr("fill", "white")
-//	.attr("x", function (d) {
-//		return d[1];
-//	})
-//	.attr("y", function (d) {
-//		return d[0];
-//	})
-//	.attr("width", 2);
-
-//Chart 3 - Dasyuromorphia (green triangle)
